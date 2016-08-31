ecd5bcd8a09103ae685fc62c0b249148354cf37b
Les Vogel <lesv@users.noreply.github.com>
2016-08-31 12:47:29 -0700

    Add gradle maven (#111)
    * Use the new plugins
    * fix script & README
    1. copyright to correct year
    2. Links to docs.
    
    * Update maven version
    
    * Add Helloworld-compat
    
    * Fix README
    
    * rm extra file; fix name
    
    * Make Tim happy...
    
    Add doing an GAE API.

--- helloworld-compat/src/main/java/com/example/appengine/helloworld/HelloServlet.java
+++ helloworld-compat/src/main/java/com/example/appengine/helloworld/HelloServlet.java
@@ -14,25 +14,38 @@
  * limitations under the License.
  */
 
-package com.example.appengine.gettingstartedjava.helloworld;
+package com.example.appengine.helloworld;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.utils.SystemProperty;
+import com.google.apphosting.api.ApiProxy;
+import com.google.apphosting.api.ApiProxy.Environment;
+
 // [START example]
 @SuppressWarnings("serial")
-@WebServlet(name = "helloworld", value = "/" )
 public class HelloServlet extends HttpServlet {
+
+  public static String getInfo() {
+    String version = SystemProperty.applicationVersion.get();
+    String majorVersion = version.substring(0, version.indexOf('.'));
+    Environment env = ApiProxy.getCurrentEnvironment();
+    String hostname =
+        "" + env.getAttributes().get("com.google.appengine.runtime.default_version_hostname");
+    String infostring = "version: " + majorVersion + " and hostname: " + hostname;
+    return infostring;
+  }
+
 
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     PrintWriter out = resp.getWriter();
-    out.println("Hello, world - Flex Servlet");
+    out.println("Hello, world - Flex Compat " + getInfo());
   }
 }
 // [END example]
