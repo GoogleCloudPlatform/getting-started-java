bd622cb2ee0995c574fbd1e3dcdad17da1dcdfca
Jerjou Cheng <jerjou@google.com>
2017-03-29 11:03:18 -0700

    Tests for #6 and gke

--- bookshelf/6-gce/src/test/java/com/example/getstarted/basicactions/UserJourneyTestIT.java
+++ bookshelf/6-gce/src/test/java/com/example/getstarted/basicactions/UserJourneyTestIT.java
@@ -16,6 +16,9 @@
 
 package com.example.getstarted.basicactions;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import com.google.cloud.datastore.Batch;
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
@@ -32,12 +35,23 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.service.DriverService;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * I can't figure out how to test server-side logging in a selenium test, so just sanity-check that
+ * it hasn't broken anything.
+ */
 @RunWith(JUnit4.class)
 @SuppressWarnings("checkstyle:abbreviationaswordinname")
 public class UserJourneyTestIT {
@@ -54,6 +68,7 @@ public class UserJourneyTestIT {
   public static void setupClass() throws Exception {
     service = ChromeDriverService.createDefaultService();
     service.start();
+
   }
 
   @AfterClass
@@ -81,9 +96,91 @@ public class UserJourneyTestIT {
     driver.quit();
   }
 
+  private WebElement checkLandingPage() throws Exception {
+    WebElement button = driver.findElement(By.cssSelector("a.btn"));
+    assertEquals("Add book", button.getText());
+
+    WebElement heading = driver.findElement(By.cssSelector("body>.container h3"));
+    assertEquals("Books", heading.getText());
+
+    WebElement list = driver.findElement(By.cssSelector("body>.container p"));
+    assertEquals("No books found", list.getText());
+
+    driver.findElement(By.linkText("Login"));
+
+    return button;
+  }
+
+  private void checkAddBookPage() throws Exception {
+    List<WebElement> inputContainers = driver.findElements(By.cssSelector("form .form-group"));
+    assertTrue("Should have more than 5 inputs", inputContainers.size() > 5);
+    assertEquals("First input should be Title",
+        "Title", inputContainers.get(0).findElement(By.tagName("label")).getText());
+    assertEquals("Second input should be Author",
+        "Author", inputContainers.get(1).findElement(By.tagName("label")).getText());
+    assertEquals("Third input should be Date Published",
+        "Date Published", inputContainers.get(2).findElement(By.tagName("label")).getText());
+    assertEquals("Fourth input should be Description",
+        "Description", inputContainers.get(3).findElement(By.tagName("label")).getText());
+
+    // The rest should be hidden
+    for (Iterator<WebElement> iter = inputContainers.listIterator(5); iter.hasNext();) {
+      WebElement el = iter.next();
+      assertTrue(el.getAttribute("class").indexOf("hidden") >= 0);
+    }
+  }
+
+  private void submitForm(String title, String author, String datePublished, String description)
+      throws Exception {
+    driver.findElement(By.cssSelector("[name=title]")).sendKeys(title);
+    driver.findElement(By.cssSelector("[name=author]")).sendKeys(author);
+    driver.findElement(By.cssSelector("[name=publishedDate]")).sendKeys(datePublished);
+    driver.findElement(By.cssSelector("[name=description]")).sendKeys(description);
+
+    driver.findElement(By.cssSelector("button[type=submit]")).submit();
+  }
+
+  private void checkReadPage(String title, String author, String datePublished, String description)
+      throws Exception {
+    WebElement heading = driver.findElement(By.cssSelector("h3"));
+    assertEquals("Book", heading.getText());
+
+    List<WebElement> buttons = driver.findElements(By.cssSelector("a.btn"));
+    assertEquals(2, buttons.size());
+    assertEquals("Edit book", buttons.get(0).getText());
+    assertEquals("Delete book", buttons.get(1).getText());
+
+    // Should be a cat thumbnail
+    assertTrue(driver.findElement(By.cssSelector("img.book-image")).getAttribute("src")
+        .indexOf("placekitten") > 0);
+    assertTrue("Should show title",
+        driver.findElement(By.cssSelector(".book-title")).getText()
+        .startsWith(title));
+    assertEquals("Should show author",
+        "By " + author, driver.findElement(By.cssSelector(".book-author")).getText());
+    assertEquals("Should show description",
+        description, driver.findElement(By.cssSelector(".book-description")).getText());
+  }
+
   @Test
   public void userJourney() throws Exception {
     driver.get("http://localhost:8080");
+
+    try {
+      WebElement button = checkLandingPage();
+
+      button.click();
+      (new WebDriverWait(driver, 10)).until(ExpectedConditions.urlMatches(".*/create$"));
+
+      checkAddBookPage();
+
+      submitForm(TITLE, AUTHOR, PUBLISHED_DATE, DESCRIPTION);
+      (new WebDriverWait(driver, 10)).until(ExpectedConditions.urlMatches(".*/read\\?id=[0-9]+$"));
+
+      checkReadPage(TITLE, AUTHOR, PUBLISHED_DATE, DESCRIPTION);
+    } catch (Exception e) {
+      System.err.println(driver.getPageSource());
+      throw e;
+    }
   }
 }
-
