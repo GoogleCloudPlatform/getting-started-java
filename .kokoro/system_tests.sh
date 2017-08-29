83e76f2e942cd68a443f4024ced0d7f161f529e1
Les Vogel <lesv@google.com>
2017-08-29 10:27:55 -0700

    should run tests now.
    1. Fail at end & quite mode => man
    2. Use the deployAll.sh script

--- .kokoro/system_tests.sh
+++ .kokoro/system_tests.sh
@@ -35,32 +35,6 @@ function TestIt() {
   grep "${3}"
 }
 
-delete_app_version() {
-  yes | gcloud --project="${GOOGLE_PROJECT_ID}" \
-    app versions delete "${1}"
-}
-
-handle_error() {
-  errcode=$? # Remember the error code so we can exit with it after cleanup
-
-  # Clean up remote app version
-  delete_app_version "${1}" &
-
-  # Display any errors
-  if [ -n "$(find "${2}" -mindepth 1 -print -quit)" ]; then
-    cat "${2:?}"/* 1>&2
-  fi
-
-  wait
-
-  exit ${errcode}
-}
-
-cleanup() {
-  delete_app_version "${GOOGLE_VERSION_ID}" &
-  ( [ -d "${ERROR_OUTPUT_DIR}" ] && rm -r "${ERROR_OUTPUT_DIR:?}/"* ) || /bin/true
-}
-
 export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/service-acct.json
 export GOOGLE_CLOUD_PROJECT=java-docs-samples-testing
 export PATH=/google-cloud-sdk/bin:$PATH
@@ -85,7 +59,7 @@ gcloud config list
 
 echo "******** build everything ********"
 cd github/getting-started-java
-mvn clean verify | grep -E -v "(^\[INFO\] Download|^\[INFO\].*skipping)"
+mvn clean verify --fail-at-end -q | grep -E -v "(^\[INFO\] Download|^\[INFO\].*skipping)"
 
 # (
 # # Stop echoing commands, so we don't leak secret env vars
@@ -120,39 +94,7 @@ gcloud auth activate-service-account\
     --key-file=$GOOGLE_APPLICATION_CREDENTIALS \
     --project=$GOOGLE_CLOUD_PROJECT
 
-# First, style-check the shell scripts
-# shellcheck ./**/*.sh
-
-for app in "helloworld" "kotlin-appengine-standard" "kotlin-springboot-appengine-standard" \
-      "springboot-appengine-standard" "kotlin-spark-appengine-standard" \
-      "sparkjava-appengine-standard"
-do
-  (cd "${app}"; \
-  mvn appengine:deploy -Dapp.deploy.version="${app}" -Dapp.deploy.project="${GOOGLE_CLOUD_PROJECT}")
-done
-
-echo "******* Test QA Deployed Apps ********"
-
-TestIt "helloworld" "" "Hello App Engine -- Java 8!"
-TestIt "helloworld" "hello" "Hello App Engine - Standard using Google App Engine"
-
-TestIt "kotlin-appengine-standard" "" \
-  "Hello, World! I am a Servlet 3.1 running on Java8 App Engine Standard, and written in Kotlin..."
-
-TestIt "kotlin-springboot-appengine-standard" "greeting" \
-  "Hello, World, from a SpringBoot Application written in Kotlin, running on Google App Engine Java8 Standard..."
-
-TestIt "springboot-appengine-standard" "" \
-  "Hello world - springboot-appengine-standard!"
-
-TestIt "kotlin-spark-appengine-standard" "" \
-  "Hello Spark Kotlin running on Java8 App Engine Standard."
-
-TestIt "kotlin-spark-appengine-standard" "hello" \
-  "Hello Spark Kotlin running on Java8 App Engine Standard."
-
-TestIt "sparkjava-appengine-standard" "" \
-  "Hello from SparkJava running on GAE Standard Java8 runtime."
+./deployAll.sh
 
 ## Find all jenkins.sh's and run them.
 # find . -mindepth 2 -maxdepth 5 -name jenkins.sh -type f | while read -r path; do
--- appengine-standard-java8/deployAll.sh
+++ appengine-standard-java8/deployAll.sh
@@ -1,9 +1,53 @@
-set +x
-gcloud config configuration activate qa
-for app in "helloworld" "kotlin-appengine-standard" "kotlin-springboot-appengine-standard" \
+set -x
+set -v
+# Temporary directory to store any output to display on error
+export ERROR_OUTPUT_DIR
+ERROR_OUTPUT_DIR="$(mktemp -d)"
+# trap 'rm -r "${ERROR_OUTPUT_DIR}"' EXIT
+URL="dot-lesv-qa-999.prom-qa.sandbox.google.com"
+
+export GOOGLE_CLOUD_PROJECT=lesv-qa-999
+export CLOUDSDK_API_ENDPOINT_OVERRIDES_APPENGINE='https://staging-appengine.sandbox.googleapis.com/'
+
+# $1 - project
+# $2 - PATH
+# $3 - search string
+function TestIt() {
+  curl -X GET "https://${1}-${URL}/${2}" | \
+  tee -a "${ERROR_OUTPUT_DIR}/response.txt" | \
+  grep "${3}"
+}
+
+# gcloud config configurations activate qa
+
+for app in "helloworld" "kotlin-appengine-standard" \
+      "kotlin-springboot-appengine-standard" \
       "springboot-appengine-standard" "kotlin-spark-appengine-standard" \
       "sparkjava-appengine-standard"
 do
-  (cd "${app}"; mvn appengine:deploy -Dapp.deploy.version="${app}" -Dapp.deploy.project="${GOOGLE_CLOUD_PROJECT}")
+  (cd "${app}"; mvn appengine:deploy -Dapp.deploy.version="${app}" \
+          -Dapp.deploy.project="${GOOGLE_CLOUD_PROJECT}" -DskipTests=true)
 done
 
+echo "******* Test QA Deployed Apps ********"
+
+TestIt "helloworld" "" "Hello App Engine -- Java 8!"
+TestIt "helloworld" "hello" "Hello App Engine - Standard using Google App Engine"
+
+TestIt "kotlin-appengine-standard" "" \
+  "Hello, World! I am a Servlet 3.1 running on Java8 App Engine Standard, and written in Kotlin..."
+
+TestIt "kotlin-springboot-appengine-standard" "greeting" \
+  "Hello, World, from a SpringBoot Application written in Kotlin, running on Google App Engine Java8 Standard..."
+
+TestIt "springboot-appengine-standard" "" \
+  "Hello world - springboot-appengine-standard!"
+
+TestIt "kotlin-spark-appengine-standard" "" \
+  "Hello Spark Kotlin running on Java8 App Engine Standard."
+
+TestIt "kotlin-spark-appengine-standard" "hello" \
+  "Hello Spark Kotlin running on Java8 App Engine Standard."
+
+TestIt "sparkjava-appengine-standard" "" \
+  "Hello from SparkJava running on GAE Standard Java8 runtime."
