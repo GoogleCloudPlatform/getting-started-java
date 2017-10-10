9a2ac75682c80bea64b100dd34506342b21544e2
Jisha Abubaker <jishaa@google.com>
2017-10-10 10:51:47 -0700

    cleanup

--- helloworld-springboot/src/main/java/com/example/java/gettingstarted/HelloworldApplication.java
+++ helloworld-springboot/src/main/java/com/example/java/gettingstarted/HelloworldApplication.java
@@ -30,8 +30,10 @@ public class HelloworldApplication {
   }
 
   /**
-   * <a href="https://cloud.google.com/appengine/docs/flexible/java/how-instances-are-managed#health_checking">
-   * App Engine health checking</a> requires responding with 200 to {@code /_ah/health}.
+   * (Optional) App Engine health check endpoint mapping.
+   * @see <a href="https://cloud.google.com/appengine/docs/flexible/java/how-instances-are-managed#health_checking"></a>
+   * If your app does not handle health checks, a HTTP 404 response is interpreted
+   *     as a successful reply.
    */
   @RequestMapping("/_ah/health")
   public String healthy() {
