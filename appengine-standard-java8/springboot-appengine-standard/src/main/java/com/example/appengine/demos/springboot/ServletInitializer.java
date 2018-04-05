f4e8e1e7815f1a45cb307df375def10b270c83f7
al-tr <al-tr@users.noreply.github.com>
2018-04-05 23:21:16 +0300

    Update import package for SpringBootServletInitializer (#377)
    Auto update bot updated spring boot v1.x -> v2.0.0, which caused some inconsistency,
    i.e. SpringBootServletInitializer has been moved from org.springframework.boot.web.support.SpringBootServletInitializer
    to org.springframework.boot.web.servlet.support.SpringBootServletInitializer
    (check: https://docs.spring.io/spring-boot/docs/2.0.0.RELEASE/api/org/springframework/boot/web/servlet/support/SpringBootServletInitializer.html)

--- appengine-standard-java8/springboot-appengine-standard/src/main/java/com/example/appengine/demos/springboot/ServletInitializer.java
+++ appengine-standard-java8/springboot-appengine-standard/src/main/java/com/example/appengine/demos/springboot/ServletInitializer.java
@@ -16,7 +16,7 @@
 package com.example.appengine.demos.springboot;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
