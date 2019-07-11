95a39fcd2dd1096ebaa52ab2f1d7b02dbafc1b2a
Dane Liergaard <dzlier@google.com>
2019-07-11 14:08:38 -0700

    New getting started on Compute Engine sample.

---
+++ gce/README.md
@@ -0,0 +1,45 @@
+# Getting Started with Java - Google Compute Engine
+
+See the [Bookshelf tutorial][tutorial] for help getting started with App Engine Standard, Cloud
+Firestore, and more.
+
+You'll need to [create a bucket][create-bucket] in Google Cloud Storage,
+referred to below as `MY-BUCKET`. You'll also need to create an OAuth2 client
+and secret, and edit `pom.xml` with its values.
+
+### Running Locally
+
+    mvn clean jetty:run-exploded -DprojectID=YOUR-PROJECT-ID
+
+### Deploying to Compute Engine
+
+* Initialize the [Google Cloud SDK][cloud_sdk]
+
+        gcloud init
+
+* In the `makeProject` script update the `BUCKET` environment variable
+  with your bucket name.
+
+* Deploy your App
+
+        ./makeProject gce
+
+* To tear down the App, use
+
+        ./makeProject down
+        
+### Deploying to Compute Engine with horizontal scaling
+
+* Initialize Google Cloud SDK and `makeProject` as above.
+
+* Deploy your App
+
+        ./makeProject gce-many
+
+* To tear down the App, use
+
+        ./makeProject down-many
+
+[tutorial]: https://cloud.google.com/java/getting-started/tutorial-app
+[create-bucket]: https://cloud.google.com/storage/docs/creating-buckets#storage-create-bucket-console
+[cloud_sdk]: https://cloud.google.com/sdk/
---
+++ gce/src/test/java/com/example/getstarted/basicactions/UserJourneyTestIT.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.getstarted.basicactions;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.service.DriverService;
+
+@RunWith(JUnit4.class)
+@SuppressWarnings("checkstyle:abbreviationaswordinname")
+public class UserJourneyTestIT {
+  private static DriverService service;
+  private WebDriver driver;
+
+  @BeforeClass
+  public static void setupClass() throws Exception {
+    service = ChromeDriverService.createDefaultService();
+    service.start();
+  }
+
+  @Before
+  public void setup() {
+    driver = new RemoteWebDriver(service.getUrl(), DesiredCapabilities.chrome());
+  }
+
+  @After
+  public void tearDown() {
+    driver.quit();
+  }
+
+  @Test
+  public void userJourney() {
+    driver.get("http://localhost:8080");
+
+    try {
+      assertTrue(driver.getPageSource().contains("Hello world - GCE!"));
+    } catch (Exception e) {
+      System.err.println(driver.getPageSource());
+      throw e;
+    }
+  }
+}
