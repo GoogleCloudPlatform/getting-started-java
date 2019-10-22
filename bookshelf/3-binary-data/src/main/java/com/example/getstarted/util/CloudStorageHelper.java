253c3019c51a7d11dee40fcd6957cc0265873c22
Frank Natividad <frankyn@users.noreply.github.com>
2019-10-22 10:27:54 -0700

    Remove deprecate Storage.create() (#441)
    * Remove deprecate Storage.create()
    
    * fix issues
    
    * Boolean conditional..
    
    * Update bookshelf/3-binary-data/src/main/java/com/example/getstarted/util/CloudStorageHelper.java
    
    Co-Authored-By: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>
    
    * Simplify solution
    
    * semicolon..
    
    * Revert and fix StringBuffer->StringBuilder
    
    * Fix testCompile warnings
    
    * Fix format issues

--- bookshelf/3-binary-data/src/main/java/com/example/getstarted/util/CloudStorageHelper.java
+++ bookshelf/3-binary-data/src/main/java/com/example/getstarted/util/CloudStorageHelper.java
@@ -23,7 +23,9 @@ import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -54,14 +56,25 @@ public class CloudStorageHelper {
    * Uploads a file to Google Cloud Storage to the bucket specified in the BUCKET_NAME
    * environment variable, appending a timestamp to end of the uploaded filename.
    */
-  @SuppressWarnings("deprecation")
+  // Note: this sample assumes small files are uploaded. For large files or streams use:
+  // Storage.writer(BlobInfo blobInfo, Storage.BlobWriteOption... options)
   public String uploadFile(Part filePart, final String bucketName) throws IOException {
     DateTimeFormatter dtf = DateTimeFormat.forPattern("-YYYY-MM-dd-HHmmssSSS");
     DateTime dt = DateTime.now(DateTimeZone.UTC);
     String dtString = dt.toString(dtf);
     final String fileName = filePart.getSubmittedFileName() + dtString;
+    
+    // The InputStream is closed by default, so we don't need to close it here
+    // Read InputStream into a ByteArrayOutputStream.
+    InputStream is = filePart.getInputStream();
+    ByteArrayOutputStream os = new ByteArrayOutputStream();
+    byte[] readBuf = new byte[4096];
+    while (is.available() > 0) {
+      int bytesRead = is.read(readBuf);
+      os.write(readBuf, 0, bytesRead);
+    }
 
-    // the inputstream is closed by default, so we don't need to close it here
+    // Convert ByteArrayOutputStream into byte[]
     BlobInfo blobInfo =
         storage.create(
             BlobInfo
@@ -69,7 +82,7 @@ public class CloudStorageHelper {
                 // Modify access list to allow all users with link to read file
                 .setAcl(new ArrayList<>(Arrays.asList(Acl.of(User.ofAllUsers(), Role.READER))))
                 .build(),
-            filePart.getInputStream());
+            os.toByteArray());
     // return the public download link
     return blobInfo.getMediaLink();
   }
